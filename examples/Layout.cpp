ca60c70fe771dcfb192571a75a5d0418957ea35b
pbrazzle <prb50@pitt.edu>
2026-02-06 13:59:00 -0800

    Layout class

--- examples/Layout.cpp
+++ examples/Layout.cpp
@@ -4,21 +4,46 @@
 
 #include "PBApp/Controls/Button.h"
 
+class Layout {
+public:
+    Layout() = default;
+
+    void add(Button* button) {
+        buttons.push_back(button);
+    }
+
+    // Equally spaced horizontally, use full height
+    void update(unsigned int width, unsigned int height) {
+        auto baseWidth = width / buttons.size();
+        auto extraWidth = width % buttons.size();
+
+        unsigned int currentX = 0;
+        for (auto button : buttons) {
+            auto bWidth = baseWidth + (extraWidth ? 1 : 0);
+            button->setSize(bWidth, height);
+            button->setPosition(currentX, 0);
+
+            extraWidth -= (extraWidth) ? 1 : 0;
+            currentX += bWidth;
+        }
+    }
+
+private:
+    std::vector<Button*> buttons;
+};
+
 class LayoutWindow : public Window {
 public:
     LayoutWindow(HWND handle) : Window(handle) {
         button1 = createButton<Button>(handle);
-        button1->setPosition(0, 0);
-
         button2 = createButton<Button>(handle);
+
+        equallySized.add(button1);
+        equallySized.add(button2);
     }
 
     void onResize(unsigned int width, unsigned int height) override {
-        auto b1Width = width / 2;
-
-        button1->setSize(b1Width, height);
-        button2->setPosition(b1Width, 0);
-        button2->setSize(width - b1Width, height);
+        equallySized.update(width, height);
     }
 
 protected:
@@ -29,6 +54,8 @@ protected:
 private:
     Button* button1;
     Button* button2;
+
+    Layout equallySized;
 };
 
 class ExampleApp : public PBApp {
